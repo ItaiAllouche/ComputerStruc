714368dcc4ad664041905a382e9992cd29bc73ec
ItaiAllouche <itai208@gmail.com>
2023-06-11 20:49:38 +0300

    passed all tests

--- HW4/run.sh
+++ HW4/run.sh
@@ -10,11 +10,12 @@ do
     var0=$( echo tests/example${num}.img)
     ./sim_main ${var0} > tests/example${num}.YoursOut
     #valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./dflow_calc ${var0} ${var1} ${var2}  > ./valgrind_tests/this_year_leak${num}.txt 2>&1
-    if cmp tests/example${num}.YoursOut tests/example${num}.out
+    if cmp tests/example${num}.YoursOut tests/example${num}.out 
         then
-    echo "${GREEN}test ${num} passed";
+        echo "${GREEN}test ${num} passed";
         else
         echo "${RED}test ${num} failed";
     fi
+    rm tests/example${num}.YoursOut
 done
 echo
